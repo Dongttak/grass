f5ddae4097beb4243c846c112183aa2c1972262b
Dongttak <109863563+Dongttak@users.noreply.github.com>
2026-03-06 14:54:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 53.64 ms, Memory: 19.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,25 +1,29 @@
 from collections import deque
 def solution(n, edge):
     answer = 0
-    A=[[] for i in range(n+1)]
+    edges=[[] for _ in range(n+1)]
     
     for i in edge:
-        A[i[0]].append(i[1])
-        A[i[1]].append(i[0])
+        edges[i[0]].append(i[1])
+        edges[i[1]].append(i[0])
+    
+    q=deque()
+    dist=[-1]*(n+1)
+    q.append(1)
+    cnt=0
+    while q:
+        now=q.popleft()
+        if dist[now]==-1: 
+            dist[now]=cnt
         
-    visited=[0]*(n+1)
-    
-    queue=deque()
-    queue.append(1)
-    visited[1]=1
-    
-    while queue:
-        now=queue.popleft()
-        for i in A[now]:
-            if not visited[i]:
-                visited[i]=visited[now]+1
-                queue.append(i)
-                
-    max_value = max(visited)
-    answer = visited.count(max_value)
+        for i in edges[now]:
+            if dist[i]==-1:
+                dist[i]=dist[now]+1
+                q.append(i)
+    maxdist=max(dist)
+    for i in dist:
+        if i==maxdist:
+            answer+=1
+        
+        
     return answer
